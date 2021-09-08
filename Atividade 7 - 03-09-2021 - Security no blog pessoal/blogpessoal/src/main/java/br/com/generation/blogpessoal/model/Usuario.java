689d0390d94657ca56f28dfa0c973eeda99de242
Raqueldcsilva <raquelshdy@gmail.com>
2021-09-08 10:19:23 -0300

    Exercício do blog pessoal atualizado

--- Atividade 7 - 03-09-2021 - Security no blog pessoal/blogpessoal/src/main/java/br/com/generation/blogpessoal/model/Usuario.java
+++ Atividade 7 - 03-09-2021 - Security no blog pessoal/blogpessoal/src/main/java/br/com/generation/blogpessoal/model/Usuario.java
@@ -1,12 +1,18 @@
 package br.com.generation.blogpessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -27,6 +33,10 @@ public class Usuario {
 	@NotNull
 	@Size(min = 5)
 	private String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 
 	public long getId() {
 		return id;
